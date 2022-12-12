ff579ec0f18160d00cc8d6988464f066bbe0d313
tjaz6 <tjazuran66@gmail.com>
2022-12-12 13:43:23 +0100

    working neural net

---
+++ video_to_image.py
@@ -0,0 +1,17 @@
+import cv2
+capture = cv2.VideoCapture('/home/tjaz/final_ass/embedded_project_ML/pos2.mp4')
+frameNr = 0
+dims = (20,20)
+
+while (True):
+    success, frame = capture.read()
+    if success:
+        frame = cv2.resize(frame, dims, interpolation = cv2.INTER_AREA)	# resize img to fit dims
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite(f'/home/tjaz/final_ass/embedded_project_ML/Output2/frame_{frameNr}.jpg', frame)
+    else:
+        break
+    frameNr = frameNr+1
+capture.release()
+
+
