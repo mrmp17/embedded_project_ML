c11196a0f91d16d4e9d84cdc3ec0eb43ae4d7345
tjaz6 <tjazuran66@gmail.com>
2022-12-13 15:38:51 +0100

    fix automatic picture gen for hls

--- image_to_array_automated.py
+++ image_to_array_automated.py
@@ -4,6 +4,7 @@ import numpy as np
 test_images = open("test_images.h","w").close()
 test_images = open("test_images.h","w")
 
+test_images.write("float images_array[20][400] = {")
 
 for i in range(1,21):
     i_str = str(i)
@@ -12,14 +13,22 @@ for i in range(1,21):
     
     image = image/255
     image = image.flatten()
-    image = str(image)
-    image =image.replace("[","{")
-    image = image.replace("]","}")
 
 
+    test_images.write("{")
 
-    test_images.write("float input_img"+ i_str + "[400] = ")
-    test_images.write(image + ";\n\n")
+    for j in range(399):
+        test_images.write(str(image[j]) + ", ")
+
+    test_images.write(str(image[399]))
+
+
+    if (i  <20):
+        test_images.write("},\t")
+
+
+    
+test_images.write("}};")
 
     
 
